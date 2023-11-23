5e0042c64759ef398dc7971e944ceaff42e4c210
jordan-reinaldo <jordan.reinaldo@laplateforme.io>
2023-11-23 11:07:49 +0100

    job05 terminé

---
+++ jour04/job05/main.py
@@ -0,0 +1,21 @@
+def list():
+    liste= [10, 20, 30, 40, 50]
+    return liste
+print(list())
+
+def list():
+    liste= [10, 20, 30, 40, 50]
+    return liste[1]
+print(list())
+
+def list():
+    liste= [10, 20, 30, 40, 50]
+    liste[3] = liste[2] + liste[4]
+    return liste
+print (list())
+
+def list():
+    liste= [10, 20, 30, 40, 50]
+    liste[3] = liste[2] + liste[4]
+    return liste[-1]
+print (list())
