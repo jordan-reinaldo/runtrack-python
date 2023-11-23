3d39851659c9066a062437ac1ae29ed1e826a582
jordan-reinaldo <jordan.reinaldo@laplateforme.io>
2023-11-23 15:51:37 +0100

    maj job 11, job12 en cours

--- jour04/job11/main.py
+++ jour04/job11/main.py
@@ -1,3 +1,14 @@
+L = [7, 11, 42, 39, 2]
+def list(liste):  
+    for i in range(len(liste)):
+        liste[i] += 1
+    return liste
+
+print(list(L)) 
+
+
+# autre façon de faire 
+
 L=[7, 11, 42, 39, 2]
 
 def list_add1(liste):
@@ -7,20 +18,3 @@ def list_add1(liste):
 L_add1 = list_add1(L)
 
 print(L_add1)
-
-# ci-dessous test de la fonction pour vérifier compréhension, on prend la nouvelle liste (celle demandé par l'exercice comme paramètre)
-
-L_add2 = list_add1(L_add1)
-
-
-print(L_add2)
-
-
-
-# autre manière de faire l'exercice :
-# L = [7, 11, 42, 39, 2]
-# longueur_liste = len(L)
-
-# for i in range (0, longueur_liste) :
-    #  L[i] = L[i] + 1
-# print (F"L modifiee par ajout de 1 : {L} ")
