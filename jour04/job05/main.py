8bde9adaf9a8568db518d54163001b3753d342cf
jordan-reinaldo <jordan.reinaldo@laplateforme.io>
2023-11-23 14:34:54 +0100

    job05 terminé

--- jour04/job05/main.py
+++ jour04/job05/main.py
@@ -1,21 +1,18 @@
-def list():
-    L= [10, 20, 30, 40, 50]
-    return L
-print(list())
+L= [10, 20, 30, 40, 50]
+def list(liste):
+    return liste
+print(list(L))
 
-def list():
-    L= [10, 20, 30, 40, 50]
-    return L[1]
-print(list())
+def list(liste):
+    return liste[1]
+print(list(L))
 
-def list():
-    L= [10, 20, 30, 40, 50]
-    L[3] = L[2] + L[4]
-    return L
-print (list())
+def list(liste):
+    liste[3] = liste[2] + liste[4]
+    return liste
+print (list(L))
 
-def list():
-    L= [10, 20, 30, 40, 50]
-    L[3] = L[2] + L[4]
-    return L[-1]
-print (list())
+def list(liste):
+    liste[3] = liste[2] + liste[4]
+    return liste[-1]
+print (list(L))
